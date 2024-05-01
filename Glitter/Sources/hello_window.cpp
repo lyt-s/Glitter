10d42b3a9611d4a140929e7c7d4c0a71e96e2d3e
lyt-s <1814666859@qq.com>
2024-05-01 15:11:13 +0800

    deleted:    Build/.gitignore 	modified:   CMakeLists.txt 	new file:   Glitter/Sources/CMakeLists.txt 	new file:   Glitter/Sources/hello_window.cpp 	deleted:    Glitter/Sources/main.cpp

---
+++ Glitter/Sources/hello_window.cpp
@@ -0,0 +1,93 @@
+// Local Headers
+#include "glitter.hpp"
+
+// System Headers
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+
+// Standard Headers
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <iterator>
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int main(int argc, char *argv[]) {
+
+  // Load GLFW and Create a Window
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+  //   glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+  auto mWindow =
+      glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", nullptr, nullptr);
+
+  // Check for Valid Context
+  if (mWindow == nullptr) {
+    std::cout << "Failed to Create OpenGL Context" << std::endl;
+    // fprintf(stderr, "Failed to Create OpenGL Context");
+    return EXIT_FAILURE;
+  }
+
+  // Create Context and Load OpenGL Functions
+  glfwMakeContextCurrent(mWindow);
+  glfwSetFramebufferSizeCallback(mWindow, framebuffer_size_callback);
+
+  // glad: load all OpenGL function pointers
+  // ---------------------------------------
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  // render loop
+  // -----------
+  while (!glfwWindowShouldClose(mWindow)) {
+    // input
+    // -----
+    processInput(mWindow);
+
+    // render
+    // ------
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved
+    // etc.)
+    // -------------------------------------------------------------------------------
+    glfwSwapBuffers(mWindow);
+    glfwPollEvents();
+  }
+
+  // glfw: terminate, clearing all previously allocated GLFW resources.
+  // ------------------------------------------------------------------
+  glfwTerminate();
+  return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this
+// frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback
+// function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  // make sure the viewport matches the new window dimensions; note that width
+  // and height will be significantly larger than specified on retina displays.
+  glViewport(0, 0, width, height);
+}
